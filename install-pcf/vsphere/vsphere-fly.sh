8b3dc7fc8ade73f0b1b299aad8f9b637db4747b3
Administrator <admin@example.com>
2017-04-20 14:05:48 -0400

    yolo

--- install-pcf/vsphere/vsphere-fly.sh
+++ install-pcf/vsphere/vsphere-fly.sh
@@ -4,5 +4,5 @@ set -e -x
 
 fly -t ci login -u concourse -p changeme -c http://10.193.98.103:8080/
 
-fly -t ci set-pipeline --pipeline install-pcf-vsphere --config pipeline.yml --load-vars-from redis.yml
+fly -t ci set-pipeline --pipeline install-pcf-vsphere --config pipeline.yml --load-vars-from .params.yml
 fly -t ci unpause-pipeline --pipeline install-pcf-vsphere
